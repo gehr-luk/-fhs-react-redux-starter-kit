6758034144e73980c91646026fb85ca2ac91f363
Lukas Gehrer <lgehrer.mmt-b2017@fh-salzburg.ac.at>
2020-03-10 15:00:30 +0100

    Add state and list to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import React from 'react'
 import SignIn from './components/SignIn'
+import List from './components/List'
 
-const App = () => <SignIn />
+class App extends React.Component {
+  state = {
+    iOweList: [
+      {
+        id: 1,
+        name: 'test0',
+        paid: false
+      },
+      {
+        id: 2,
+        name: 'test1',
+        paid: false
+      },
+      {
+        id: 3,
+        name: 'test2',
+        paid: false
+      }
+    ],
+    owesMeList: [
+      {
+        id:1,
+        name: 'somebody',
+        paid: false
+      }
+    ]
+  }
+  render () {
+    return <div><SignIn /><List iOweList={this.state.iOweList} /></div>
+  }
+}
 
 export default App
