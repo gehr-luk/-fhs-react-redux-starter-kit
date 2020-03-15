daec3cb919669c469f83b5cbdb8509f8d364e384
Lukas Gehrer <lgehrer.mmt-b2017@fh-salzburg.ac.at>
2020-03-15 22:37:37 +0100

    Remove list from App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import React from 'react'
 import SignIn from './components/SignIn'
-import List from './components/List'
 
 const App = () => {
-  const state = {
-    iOweList: [
-      {
-        id: 1,
-        name: 'test0',
-        paid: false
-      },
-      {
-        id: 2,
-        name: 'test1',
-        paid: false
-      },
-      {
-        id: 3,
-        name: 'test2',
-        paid: false
-      }
-    ],
-    owesMeList: [
-      {
-        id: 1,
-        name: 'somebody',
-        paid: false
-      }
-    ]
-  }
-  return <div><SignIn /><List iOweList={state.iOweList} /></div>
+  return <div><SignIn /></div>
 }
 
 export default App
