2e5e05b1d07b8c00439fe960dfe40a337816aa29
Lukas Gehrer <lgehrer.mmt-b2017@fh-salzburg.ac.at>
2020-03-16 13:40:18 +0100

    Add styles for line through text and invisible button

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,13 +3,15 @@ import style from './transaction.module.css'
 
 const ListItem = ({ iOweListItem }) => {
   const amount = iOweListItem.amount.toString().replace('.', ',')
-
+  const buttonInvisible = iOweListItem.paid ? style.buttonInvisible : ''
+  const textLineThrough = iOweListItem.paid ? style.textLineThrough : ''
+  
   return (
     <div className={style.listItemContainer}>
       <li className={style.listItem}>
-        <p className={`${style.name} ${style.listItemPart}`}>{iOweListItem.name}</p>
-        <p className={`${style.amount} ${style.listItemPart}`}>{amount}$</p>
-        <button className={`${style.paidButton} ${style.listItemPart}`}>Paid</button>
+        <p className={`${style.name} ${style.listItemPart} ${textLineThrough}`}>{iOweListItem.name}</p>
+        <p className={`${style.amount} ${style.listItemPart} ${textLineThrough}`}>{amount}$</p>
+        <button className={`${style.paidButton} ${style.listItemPart} ${buttonInvisible}`}>Paid</button>
       </li>
     </div>
   )
