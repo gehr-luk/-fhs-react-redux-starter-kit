8b493d8651a3ee858f7f0f7685ac0e67107208c6
Lukas Gehrer <lgehrer.mmt-b2017@fh-salzburg.ac.at>
2020-03-22 15:38:57 +0100

    Fix styling of createMoneyTransaction

--- src/components/CreateButton.js
+++ src/components/CreateButton.js
@@ -3,7 +3,7 @@ import buttonStyles from './Button.module.css'
 import createButtonStyles from './CreateMoneyTransaction.module.css'
 
 const CreateButton = ({ onClick, children }) => (
-  <button onClick={onClick} className={`${buttonStyles.button} ${buttonStyles.primary} ${createButtonStyles.createButton}`}>
+  <button onClick={onClick} className={`${buttonStyles.button} ${createButtonStyles.createButton}`}>
     {children}
   </button>
 )
